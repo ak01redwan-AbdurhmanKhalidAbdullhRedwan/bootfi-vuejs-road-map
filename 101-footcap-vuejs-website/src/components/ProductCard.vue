81db153ba3c1659810442dff7e3370526dccee8e
ak01redwan-AbdurhmanKhalidAbdullhRedwan <ak01redwan@gmail.com>
2024-11-13 14:16:30 +0300

    Create components for ProductsSection and ProductCard and minimize code using loops and arrays

---
+++ 101-footcap-vuejs-website/src/components/ProductCard.vue
@@ -0,0 +1,83 @@
+<template>
+    <li class="product-item">
+        <div class="product-card" tabindex="0">
+
+            <figure class="card-banner">
+                <img :src="image" width="312" height="350" loading="lazy" :alt="title"
+                    class="image-contain">
+
+                <div v-show="badge" class="card-badge">{{ badge }}</div>
+
+                <ul class="card-action-list">
+
+                    <li class="card-action-item">
+                        <button class="card-action-btn" aria-labelledby="card-label-1">
+                            <ion-icon name="cart-outline"></ion-icon>
+                        </button>
+
+                        <div class="card-action-tooltip" id="card-label-1">Add to Cart</div>
+                    </li>
+
+                    <li class="card-action-item">
+                        <button class="card-action-btn" aria-labelledby="card-label-2">
+                            <ion-icon name="heart-outline"></ion-icon>
+                        </button>
+
+                        <div class="card-action-tooltip" id="card-label-2">Add to Whishlist</div>
+                    </li>
+
+                    <li class="card-action-item">
+                        <button class="card-action-btn" aria-labelledby="card-label-3">
+                            <ion-icon name="eye-outline"></ion-icon>
+                        </button>
+
+                        <div class="card-action-tooltip" id="card-label-3">Quick View</div>
+                    </li>
+
+                    <li class="card-action-item">
+                        <button class="card-action-btn" aria-labelledby="card-label-4">
+                            <ion-icon name="repeat-outline"></ion-icon>
+                        </button>
+
+                        <div class="card-action-tooltip" id="card-label-4">Compare</div>
+                    </li>
+
+                </ul>
+            </figure>
+
+            <div class="card-content">
+
+                <div class="card-cat">
+                    <a v-for="category in categories" 
+                        :key="category.link" :href="category.link" class="card-cat-link"> 
+                        {{category.text }}
+                        {{ category.link != categories[categories.length-1].link ? '/' : ' ' }}
+                    </a>
+                </div>
+
+                <h3 class="h3 card-title">
+                    <a :href="link">{{ title }}</a>
+                </h3>
+
+                <data class="card-price" value="180.85">${{ price }}</data>
+
+            </div>
+
+        </div>
+    </li>
+</template>
+
+<script>
+
+
+export default {
+    props: {
+        badge: {type: String, default: ''},
+        image: {type: Object, default: null},
+        title: {type: String, default: 'Card Title'},
+        link: {type: String, default: ''},
+        price: {type: Number, default: 0},
+        categories: {type: Array, required: true}
+    }
+}
+</script>
---
+++ 101-footcap-vuejs-website/src/components/ProductsSection.vue
@@ -0,0 +1,109 @@
+<template>
+    <section class="section product">
+        <div class="container">
+
+            <h2 class="h2 section-title">Bestsellers Products</h2>
+
+            <ul class="filter-list">
+                <li v-for="filterItem in filterList" :key="filterItem.id">
+                    <button 
+                        onclick="alert('this should complete to filter based on item company')" 
+                        :class="{'active': currentActiveItem == filterItem.id}" class="filter-btn"
+                    >
+                        {{ filterItem.text }}
+                    </button>
+                </li>
+            </ul>
+
+            <ul class="product-list">
+                <ProductCard 
+                    v-for="product in products" 
+                    :key="product.link"
+                    :badge="product.badge"
+                    :image="product.image"
+                    :title="product.title"
+                    :link="product.link"
+                    :price="product.price"
+                    :categories="product.categories"
+                />
+            </ul>
+
+        </div>
+    </section>
+</template>
+
+<script>
+import { ref } from 'vue';
+import ProductCard from './ProductCard.vue';
+import product1 from '@/assets/images/product-1.jpg';
+import product2 from '@/assets/images/product-2.jpg';
+import product3 from '@/assets/images/product-3.jpg';
+import product4 from '@/assets/images/product-4.jpg';
+import product5 from '@/assets/images/product-5.jpg';
+import product6 from '@/assets/images/product-6.jpg';
+import product7 from '@/assets/images/product-7.jpg';
+import product8 from '@/assets/images/product-8.jpg';
+
+export default {
+    name: 'ProductsSection',
+    components: {
+        ProductCard
+    },
+    setup() {
+        const products = ref([
+            {badge: 'New', image: product1, title: 'Running Sneaker Shoes', link: '#RunningSneakerShoes', price: 210.50, categories:[
+                {link: '#man', text: 'Man'},
+                {link: '#women', text: 'Women'}
+            ]}
+            ,
+            {badge: '', image: product2, title: 'Leather Mens Slipper', link: '#LeatherMensSlipper', price: 190.85, categories:[
+                {link: '#man', text: 'Man'},
+                {link: '#sports', text: 'Sports'}
+            ]}
+            ,
+            {badge: 'New', image: product3, title: 'Simple Fabric Shoe', link: '#SimpleFabricShoe', price: 160.85, categories:[
+                {link: '#man', text: 'Man'},
+                {link: '#women', text: 'Women'}
+            ]}
+            ,
+            {badge: '-25%', image: product4, title: 'Air Jordan 7 Retro', link: '#AirJordan7Retro44', price: 169.99, categories:[
+                {link: '#man', text: 'Man'},
+                {link: '#sports', text: 'Sports'}
+            ]}
+            ,
+            {badge: 'New', image: product5, title: 'Nike Air Max 270 SE', link: '#NikeAirMax270SE', price: 120.85, categories:[
+                {link: '#man', text: 'Man'},
+                {link: '#women', text: 'Women'}
+            ]}
+            ,
+            {image: product6, title: 'Adidas Sneakers Shoes', link: '#AdidasSneakersShoes', price: 100.50, categories:[
+                {link: '#man', text: 'Man'},
+                {link: '#women', text: 'Women'}
+            ]}
+            ,
+            {image: product7, title: 'Nike Basketball shoes', link: '#NikeBasketballshoes', price: 120.85, categories:[
+                {link: '#men', text: 'Men'},
+                {link: '#sport', text: 'Sport'}
+            ]}
+            ,
+            {image: product8, title: 'Simple Fabric Shoe', link: '#SimpleFabricShoe', price: 100.85, categories:[
+                {link: '#men', text: 'Men'},
+                {link: '#women', text: 'Women'}
+            ]}
+        ]);
+
+
+        const filterList = ref([
+            {id: 1, text: 'All', compony: 'all'},
+            {id: 2, text: 'Nike', compony: 'nike'},
+            {id: 3, text: 'Adidas', compony: 'adidas'},
+            {id: 4, text: 'Puma', compony: 'puma'},
+            {id: 5, text: 'Bata', compony: 'bata'},
+            {id: 6, text: 'Apex', compony: 'apex'}
+        ]);
+        let currentActiveItem = 1;
+
+        return {products, filterList, currentActiveItem}
+    }
+}
+</script>
