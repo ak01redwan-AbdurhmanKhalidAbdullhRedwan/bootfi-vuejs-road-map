90f2bc75087c2978ad2fea3e22ebcad1be6ee831
ak01redwan-AbdurhmanKhalidAbdullhRedwan <ak01redwan@gmail.com>
2024-11-14 09:55:50 +0300

    Create CTAsSection and minimize it's code

--- 101-footcap-vuejs-website/src/App.vue
+++ 101-footcap-vuejs-website/src/App.vue
@@ -17,49 +17,8 @@
          <ProductsSection />
 
 
-        <!-- 
-        - #CTA
-      -->
-
-        <section class="section cta">
-          <div class="container">
-
-            <ul class="cta-list">
-
-              <li>
-                <div class="cta-card" style="background-image: url('./assets/images/cta-1.jpg')">
-                  <p class="card-subtitle">Adidas Shoes</p>
-
-                  <h3 class="h2 card-title">The Summer Sale Off 50%</h3>
-
-                  <a href="#" class="btn btn-link">
-                    <span>Shop Now</span>
-
-                    <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
-                  </a>
-                </div>
-              </li>
-
-              <li>
-                <div class="cta-card" style="background-image: url('./assets/images/cta-2.jpg')">
-                  <p class="card-subtitle">Nike Shoes</p>
-
-                  <h3 class="h2 card-title">Makes Yourself Keep Sporty</h3>
-
-                  <a href="#" class="btn btn-link">
-                    <span>Shop Now</span>
-
-                    <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
-                  </a>
-                </div>
-              </li>
-
-            </ul>
-
-          </div>
-        </section>
-
-
+        <!-- CTA Section -->
+         <CtaSection />
 
 
 
@@ -761,6 +720,7 @@ import MainHeader from './components/MainHeader.vue';
 import HeroSection from './components/HeroSection.vue';
 import CollectionsSection from './components/CollectionsSection.vue';
 import ProductsSection from './components/ProductsSection.vue';
+import CtaSection from './components/CtaSection.vue';
 
 export default {
   name: 'eCommerceFootcapWebsiteBootFiVuejsRoadMap',
@@ -768,7 +728,8 @@ export default {
     MainHeader,
     HeroSection,
     CollectionsSection,
-    ProductsSection
+    ProductsSection,
+    CtaSection
   },
   setup() {
   }
---
+++ 101-footcap-vuejs-website/src/components/CtaSection.vue
@@ -0,0 +1,41 @@
+<template>
+    <section class="section cta">
+        <div class="container">
+            <ul class="cta-list">
+
+                <li v-for="cta in CTAs" :key="cta.id">
+                    <div class="cta-card" :style="{backgroundImage: `url(${cta.image})`}">
+                        <p class="card-subtitle">{{ cta.title }}</p>
+
+                        <h3 class="h2 card-title">{{ cta.mainTitle }}</h3>
+
+                        <a :href="cta.link" class="btn btn-link">
+                            <span>{{ cta.linkLabel }}</span>
+
+                            <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
+                        </a>
+                    </div>
+                </li>
+
+            </ul>
+
+        </div>
+    </section>
+</template>
+
+<script>
+import cta1JPG from '@/assets/images/cta-1.jpg';
+import cta2JPG from '@/assets/images/cta-2.jpg'
+import { reactive } from 'vue';
+
+export default {
+    setup() {
+        const CTAs = reactive([
+            {id: 1, image: cta1JPG, title: 'Adidas Shoes', mainTitle: 'The Summer Sale Off 50%', linkLabel: 'Shop Now', link: '#AdidasShoes'},
+            {id: 2, image: cta2JPG, title: 'Nike Shoes', mainTitle: 'Makes Yourself Keep Sporty', linkLabel: 'Shop Now', link: '#NikeShoes'}
+        ]);
+
+        return {cta1JPG, cta2JPG, CTAs}
+    }
+}
+</script>
